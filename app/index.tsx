3d9fc2c9581b4b3019ec1c4504fc62babee94a28
Daniel Otieno <danielotieno.ke@gmail.com>
2025-03-08 18:11:31 +0300

    setup styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1 items-center justify-center bg-gray-100'>
+      <Text className='text-5xl text-blue-600'>Hello World</Text>
     </View>
   );
 }
